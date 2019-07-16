aafe6a58d5243eb70fbce427cb203df365e0cb45
Akiraf82 <52958319+Akiraf82@users.noreply.github.com>
2019-07-16 13:41:10 +0200

    insertion du code header

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+		<meta charset="utf-8" /> 
+        <link rel="stylesheet" href="style.css" />
+		<title>Saisie Code Lot</title>
+	</head>
+    
+	
+<body>
+	
+	<header>
+		
+		<h2> Melons Soldive - Le plaisir Partagé </h2>
+		
+	
+	</header>
